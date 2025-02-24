d6b8684f80a7667d04ea103bd6191e52ae814cca
Yury Silvestrov-Henocq <yursilv@yahoo.fr>
2025-02-24 10:59:13 +0100

    useless change reverted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { MdePopoverModule } from '../../projects/material-extended/mde/src/lib/popover';
+import { MdePopoverModule } from '@jaguards/material-extended-mde';
 
 import { AppRoutingModule } from './app-routing.module';
 
